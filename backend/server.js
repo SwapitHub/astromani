27722845115ad6e144e53d09542b85d27760cb10
shamsher singh <shamshersingh8574@gmail.com>
2025-11-13 17:43:58 +0530

    13-11-2025

--- backend/server.js
+++ backend/server.js
@@ -106,6 +106,7 @@ app.use(express.json());
 
 // connectMongoDb("mongodb://localhost:27017/chatting");
 
+
 connectMongoDb(
   "mongodb+srv://swapitshamsher:Eb25QUq9aEt27aSQ@astrologer.euynurr.mongodb.net/astromani?retryWrites=true&w=majority"
 );
