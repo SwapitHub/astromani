4d596323700f74c836f621aea4c9234adcf7cc23
shamsher singh <shamshersingh8574@gmail.com>
2025-11-18 17:26:14 +0530

    18-11-2025

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 
 async function connectMongoDb(MONGO_URI) {
     return mongoose
-    .connect(MONGO_URI)
+    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB!"))
     .catch((error) => console.error("Error connecting to MongoDB:", error));
 }
